8506965e68ae8c981ad122afbbb5991356a6e35a
Eixix <tobiasbetz@hotmail.de>
2022-07-03 19:09:06 +0200

    Added lines

--- install.sh
+++ install.sh
@@ -14,12 +14,6 @@ yes | pacman -Syu
 yes | pamac install git neovim numlockx yubikey-manager-qt yubikey-personalization-gui yubioath-desktop whatsapp-for-linux visual-studio-code-bin spotify thunderbird telegram-desktop signal-desktop google-chrome dolphin direnv exa neovim
 yes | sudo -u $USER sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-sudo -u $USER rm $HOME/.zshrc $HOME/.Xresources
-sudo -u $USER ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-sudo -u $USER ln -s $HOME/.dotfiles/.Xresources $HOME/.Xresources
-sudo -u $USER rm $HOME/.i3/config
-sudo -u $USER ln -s $HOME/.dotfiles/config $HOME/.i3/config
-
 # GPG keys
 KEYID="0x4b7228cfe59b7380"
 sudo -u $USER gpg --recv $KEYID
@@ -28,6 +22,14 @@ sudo -u $USER echo -e "5\ny\n" | gpg --command-fd 0 --edit-key "$KEYID" trust
 # Configure for git
 sudo -u $USER git config --global user.signingkey "$KEYID"
 sudo -u $USER git config --global commit.gpgsign true
+
+# Clone from repository
+git clone git@github.com:Eixix/.dotfiles.git
+sudo -u $USER rm $HOME/.zshrc $HOME/.Xresources
+sudo -u $USER ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+sudo -u $USER ln -s $HOME/.dotfiles/.Xresources $HOME/.Xresources
+sudo -u $USER rm $HOME/.i3/config
+sudo -u $USER ln -s $HOME/.dotfiles/config $HOME/.i3/config
 
 # Add Yubikey PAM auth to all configs
 PAM_LINE="auth sufficient pam_u2f.so"
